67041056d37e0544c9ea0487736e8e466b2ed3e7
Glauber Ferreira <glauber.ferreira@ifal.edu.br>
2024-03-11 15:48:18 -0300

    implementa a soma de dois números

--- Calculadora.js
+++ Calculadora.js
@@ -1,8 +1,19 @@
-import { Text } from 'react-native';
+import { useState } from 'react';
+import { Button, Text, TextInput, View } from 'react-native';
 
 const Calculadora = () => {
+    const [parcela1, setParcela1] = useState(0);
+    const [parcela2, setParcela2] = useState(0);
+    const [soma, setSoma] = useState(0);
+
     return (
-        <Text>Calculadora</Text>
+        <View>
+            <Text>Calculadora</Text>
+            <TextInput defaultValue={parcela1.toString()} onChangeText={(texto) => setParcela1(Number(texto))}/>
+            <TextInput defaultValue={parcela2.toString()} onChangeText={(texto) => setParcela2(Number(texto))}/>
+            <Button title="Somar" onPress={() => setSoma(parcela1 + parcela2)}/>
+            <Text>Soma: {soma}</Text>
+        </View>
     );
 }
 
