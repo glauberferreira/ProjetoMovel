6555c9373c028af0b56444fb4bd93fa768529a9b
Glauber Ferreira <glauber.ferreira@ifal.edu.br>
2024-07-02 14:32:31 -0300

    atualiza versão dos pacotes + outras mudanças

--- app/calculadora.js
+++ app/calculadora.js
@@ -1,21 +1,32 @@
 import { useState } from 'react';
-import { Button, Text, TextInput, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
 import Estilos from '../Estilos';
+import { IconButton, MD3Colors, PaperProvider, TextInput } from 'react-native-paper';
 
 const Calculadora = () => {
     const [parcela1, setParcela1] = useState(0);
     const [parcela2, setParcela2] = useState(0);
     const [soma, setSoma] = useState(0);
+    const [login, setLogin] = useState("login");
 
     return (
-        <View>
-            <Text style={Estilos.container}>Calculadora</Text>
-            <TextInput maxLength={3} defaultValue={parcela1.toString()} onChangeText={(texto) => setParcela1(Number(texto))}/>
-            <TextInput defaultValue={parcela2.toString()} onChangeText={(texto) => setParcela2(Number(texto))}/>
-            <Button title="Somar" onPress={() => setSoma(parcela1 + parcela2)}/>
-            <Text>Soma: {soma}</Text>
-            <TextInput returnKeyType='send'/>
-        </View>
+        <PaperProvider>
+            <View>
+                <Text style={Estilos.container}>Calculadora</Text>
+                <TextInput left={<TextInput.Icon icon="eye" onPress={() => console.log('Pressed olho')} color={login === "email" ? MD3Colors.error50 : MD3Colors.error100}/>} right={<TextInput.Icon icon="eye"/>} maxLength={3} defaultValue={parcela1.toString()} onChangeText={(texto) => setParcela1(Number(texto))}/>
+                <TextInput defaultValue={parcela2.toString()} onChangeText={(texto) => setParcela2(Number(texto))}/>
+                <Button title="Somar" onPress={() => setSoma(parcela1 + parcela2)}/>
+                <Text>Soma: {soma}</Text>
+                <TextInput returnKeyType='send'/>
+                <IconButton
+    icon="camera"
+    iconColor={MD3Colors.error50}
+    size={20}
+    disabled={false}
+    
+  />
+            </View>
+        </PaperProvider>
     );
 }
 
--- app/cliques.js
+++ app/cliques.js
@@ -1,14 +1,15 @@
 import { useState } from 'react';
-import { Button, Text, View } from "react-native";
+import { Text, View } from "react-native";
+import { Button } from 'react-native-paper';
 
 const Cliques = () => {
     const [getCliques, setCliques] = useState(0);
 
     return(
         <View>
-            <Button title='OK' onPress={() => {
+            <Button icon="send" mode="contained" onPress={() => {
                 setCliques(getCliques + 1)
-            }}/>
+            }}>OK</Button>
             <Text>Quantidade de cliques: {getCliques}</Text>    
         </View>
     )
