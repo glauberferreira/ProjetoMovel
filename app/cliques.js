b9451a92dd618e1dd9caacfc82f23454d78234c2
Glauber Ferreira <glauber.ferreira@ifal.edu.br>
2024-07-23 16:08:36 -0300

    exibir os dados do usuário em uma tela logada

--- app/cliques.js
+++ app/cliques.js
@@ -1,15 +1,25 @@
 import { useState } from 'react';
 import { Text, View } from "react-native";
 import { Button } from 'react-native-paper';
+import { app } from '../firebaseConfig';
+import { getAuth } from 'firebase/auth';
 
 const Cliques = () => {
     const [getCliques, setCliques] = useState(0);
+    const auth = getAuth(app);
+    const user = auth.currentUser;
 
     return(
         <View>
             <Button icon="send" mode="contained" onPress={() => {
                 setCliques(getCliques + 1)
             }}>OK</Button>
+            <Text>{user.uid}</Text>
+            <Text>{user.email}</Text>
+            <Text>{user.displayName}</Text>
+            <Text>{user.phoneNumber}</Text>
+            <Text>{user.photoURL}</Text>
+            <Text>{user.emailVerified}</Text>
             <Text>Quantidade de cliques: {getCliques}</Text>    
         </View>
     )
